bd5242a50b84fc8fa6045289f84ef7fe00fff5ac
Hyun996 <kha_96@naver.com>
2019-10-12 14:01:13 +0900

    [기초-산술연산] 정수 2개 입력받아 합 출력하기2(설명)

---
+++ C/1039.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    long long int a = 0, b = 0;
+    long long int sum = 0;
+    scanf("%lld %lld", &a, &b);
+    sum = a + b;
+    printf("%lld", sum);
+    return 0;
+}
