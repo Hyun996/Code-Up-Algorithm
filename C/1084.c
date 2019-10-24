bc1c08ae06858bdbefc066df6dfb7ffe0bee8da2
Hyun996 <kha_96@naver.com>
2019-10-24 20:06:29 +0900

    [기초-종합] 빛 섞어 색 만들기(설명)

---
+++ C/1084.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0, j = 0, k = 0, c = 0;
+    int r = 0, g = 0, b = 0;
+    scanf("%d %d %d", &r, &g, &b);
+    for(i=0; i<r; i++){
+        for(j=0; j<g; j++){
+            for(k=0; k<b; k++){
+                printf("%d %d %d\n", i, j, k);
+            }
+        }
+    }
+    printf("%d", i*j*k);
+    return 0;
+}
+
